f961f7cb6adf0da1ccd12c06efe70dbc43fb01ee
natitivi <nati.orobles@gmail.com>
2022-07-10 00:50:44 -0500

    version 1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name= 'HOME'
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Discover the new <strong>{name}</strong> 
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          2022
         </a>
       </header>
     </div>
